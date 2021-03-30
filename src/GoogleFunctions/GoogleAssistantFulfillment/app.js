5f153bf5f397cecb69a2b48bd96e8104c2ecff8c
Barry Wheeler <wheelerbarry@hotmail.com>
2021-03-30 22:33:48 +0100

    More random tweaks

--- src/GoogleFunctions/GoogleAssistantFulfillment/app.js
+++ src/GoogleFunctions/GoogleAssistantFulfillment/app.js
@@ -5,11 +5,85 @@ const functions = require('firebase-functions');
 const http = require('https');
 const querystring = require('querystring');
 const request1 = require('sync-request');
+    const moment = require('moment');
 
 const app = conversation();
 
     function somethingelse(collections) {
+        var now = moment().utc("2021-03-30T20:00:00");
+        var tommorow = moment().utc().startOf('day').add(1, 'day');
+        var next = moment().utc().startOf('day').add(7, 'days');
+        var lookahead = moment().utc().startOf('day').add(7, 'days');
 
+        var collectionFound = false;
+
+        collections.forEach(function (arrayItem) {
+
+            var collectionDate = moment.utc(arrayItem.Date).add(15, 'hours');
+
+            if ((collectionDate > now) && (collectionDate < lookahead)) {
+                collectionFound = true;
+
+                var dayOfWeek = collectionDate.format('dddd');
+
+                var thing = "";
+
+                if (collectionDate.isSame(now, 'day')) {
+                    // check if today
+                    thing = "Today";
+                }
+                else if (collectionDate.isSame(tommorow, 'day')) {
+                    // check if tomorrow
+                    thing = "Tomorrow";
+                }
+                else {
+                    // This can happen if it is after the cutoff, so say next to avoid confusion thinking it is today.
+                    if (collectionDate.isSame(next, 'day')) {
+                        thing = "Next ";
+                    }
+                    else {
+                        thing = "On ";
+                    }
+
+                    // The day of the week.
+                    thing += collectionDate.format('dddd');
+                }
+
+                thing += " your ";
+
+                arrayItem.Bins.forEach(function (bin, index) {
+
+                    if (index > 0) {
+                        if (index === arrayItem.Bins.length - 1) {
+                            thing += " and ";
+                        }
+                        else {
+                            thing += ", ";
+                        }
+                    }
+
+                    thing += bin.Colour;
+
+                });
+
+                if (arrayItem.Bins.length === 1) {
+                    thing += " bin ";
+                }
+                else {
+                    thing += " bins "
+                }
+
+                thing += "will be collected."
+            }
+            else if ((collectionDate.isSame(now, 'day'))) {
+                // Collection was earlier today, so increase the lookahead and use next.
+                lookahead.add(1, 'day');
+            }
+        });
+
+        if (collectionFound === false) {
+            var thing = "There are no collections in the next week";
+        }
     };
 
 
@@ -36,18 +110,18 @@ const app = conversation();
         method: 'GET'
     };
 
-
+    var f = moment().startOf('day');
 
     //const res1 = request1('GET', "https://europe-west2-what-bins-708d7.cloudfunctions.net/lookup?postcode=PR7%207HB");
 
     //var user = JSON.parse(res1.getBody('utf8'));
 
-    var data = "{\"State\":\"Collection\",\"Collections\":[{\"Date\":\"2021-04-02\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-04-09\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"},{\"Colour\":\"Brown\"}]},{\"Date\":\"2021-04-16\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-04-23\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"}]},{\"Date\":\"2021-04-30\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-05-07\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"},{\"Colour\":\"Brown\"}]},{\"Date\":\"2021-05-14\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-05-21\",\"Bins\":[{\"Colour\":\"Blue\"}]}]}";
+    var data = "{\"State\":\"Collection\",\"Collections\":[{\"Date\":\"2021-03-30\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-04-06\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"},{\"Colour\":\"Brown\"}]},{\"Date\":\"2021-04-16\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-04-23\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"}]},{\"Date\":\"2021-04-30\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-05-07\",\"Bins\":[{\"Colour\":\"Blue\"},{\"Colour\":\"Grey\"},{\"Colour\":\"Brown\"}]},{\"Date\":\"2021-05-14\",\"Bins\":[{\"Colour\":\"Green\"}]},{\"Date\":\"2021-05-21\",\"Bins\":[{\"Colour\":\"Blue\"}]}]}";
 
 
     const lookupResult = JSON.parse(data);
 
-    ssomething(lookupResult);
+    something(lookupResult);
 
     var i = 0;
 
